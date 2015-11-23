d588699a22a3c614dbade32f3427199b69ab3e55
Marc Schoolderman <squell@xs4all.nl>
2015-11-24 00:25:18 +0100

    detection of wrapping extern "C"/"C++" blocks around includes

--- timehdr.sh
+++ timehdr.sh
@@ -24,15 +24,14 @@ SAMPLE=3	# how many trial compilations to run for every measurement?
 PAR=2		# how many compilations to run in parallel?
 SELECT=1,2	# which results to select for averaging? (sed address expression)
 
-AGGREGATE="i guess this is okay"
-
-transaction="/tmp/cpp$$.ii"
+transaction="/tmp/timehdr$$.ii"
 indent=0
 prev=0
-externc=""
+externc=0
 marker='|'
 
 measurement () {
+	if [ "$externc" -gt 0 ]; then printf "%${externc}s\n" "" | tr ' ' '}' >> "$transaction"; fi
 	prev="$elapsed"
 	elapsed=$(
 		for ((t=1; t<=PAR; t++)); do
@@ -40,6 +39,7 @@ measurement () {
 			(time -p $CC $FLAGS "$transaction" -o /dev/null 2> /dev/null || kill -- -$$) 2>&1 | grep "^$TIMECAT" | tr -cd '[0-9]\n'
 		done & done | sort -nk2 | sed -n "${SELECT}s/^0*//p" | awk '{ sum+=$1 } END { print int(0.5+sum/NR) }'
 	)
+	if [ "$externc" -gt 0 ]; then sed -i '$d' "$transaction"; fi
 }
 
 incremental_compilation() {
@@ -51,26 +51,18 @@ incremental_compilation() {
                         if [ "${code##\#[! ]*}" = "" ]; then
                                 # ignore pragmas and other stuff
                                 continue
-                        elif [ "${code##*4*}" = "" ]; then
-                                externc=yes
-                                continue
                         elif [ "${code##*1*}" = "" ]; then
                                 hdr="${line%[\"]*}"
                                 printf "\t%$((indent*4))s$hdr\n" ""
-				if [ "$AGGREGATE" ]; then
-					# these outer parentheses are very important!
-					(indent=$((indent+1)); incremental_compilation)
-				else
-					indent=$((indent+1)); incremental_compilation
-				fi
+				# these outer parentheses are very important!
+				(indent=$((indent+1)); incremental_compilation)
 				measurement
 				printf "%6d\t%$((indent*4))s%+d\n" "$elapsed" "" "$((elapsed-prev))"
-                        elif [ "${code##*2*}" = "" ] && ! [ "$externc" ]; then
-                                indent=$((indent-1))
+                        elif [ "${code##*2*}" = "" ]; then
 				return
                         fi
-                        externc=""
-                        #hdr="${line%[\"]*}"
+		elif [ "${line##extern*\"C*\{}" != "$line" ]; then
+			externc=$((externc+1))
                 fi
         done
 }
