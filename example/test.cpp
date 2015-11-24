50cc0b658fa565dbe6d86890f278ea94326c9f24
Marc Schoolderman <mschool@science.ru.nl>
2015-11-24 13:17:27 +0100

    example

---
+++ example/test.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <map>
+#include <stdexcept>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+}
